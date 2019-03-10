3bbc63ff7952e78e12bf20ec59038a5957942c00
Du Yuyang <du.yuyang@rea-group.com>
2019-03-11 09:40:33 +1100

    how heapq works

--- data_structure/heapq_example.py
+++ data_structure/heapq_example.py
@@ -1,5 +1,11 @@
-# Python code to demonstrate working of  
-# heapify(), heappush() and heappop() 
+# my understanding of heap
+
+## heap seems to be a list but always being ordered in a way
+## push will insert a new element to the list, then re-order
+## pop will remove the smallest
+
+
+# Some Python code to demonstrate
   
 # importing "heapq" to implement heap queue 
 import heapq 
@@ -25,3 +31,23 @@ print (list(li))
 # using heappop() to pop smallest element 
 print ("The popped and smallest element is : ",end="") 
 print (heapq.heappop(li)) 
+
+# using heappushpop() to push and pop items simultaneously 
+# pops 2 
+print ("The popped item using heappushpop() is : ",end="") 
+print (heapq.heappushpop(li, 2)) 
+  
+# using heapreplace() to pop and push items simultaneously 
+# pops 3 
+print ("The popped item using heapreplace() is : ",end="") 
+print (heapq.heapreplace(li, 2)) 
+
+# using nlargest to print 3 largest numbers 
+# prints 10, 9 and 8 
+print("The 3 largest numbers in list are : ",end="") 
+print(heapq.nlargest(3, li)) 
+  
+# using nsmallest to print 3 smallest numbers 
+# prints 1, 3 and 4 
+print("The 3 smallest numbers in list are : ",end="") 
+print(heapq.nsmallest(3, li)) 

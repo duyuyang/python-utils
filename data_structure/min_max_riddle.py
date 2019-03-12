9ee9cc38c026c1e3f4cee1c666ba05f223592b29
Du Yuyang <du.r.yuyang@gmail.com>
2019-03-12 21:38:13 +1100

    working solution

--- data_structure/min_max_riddle.py
+++ data_structure/min_max_riddle.py
@@ -10,13 +10,37 @@ def getMax(arr):
   heapq.heapify(arr)
   return heapq.nlargest(1, arr)[0]
 
+# def riddle(arr):
+#   for i in range(len(arr)):
+#     size = i + 1
+#     winMins = deque()
+#     for j in range(len(arr) - i):
+#       winMins.append(getMin(arr[j:j+size]))
+#     yield getMax(list(winMins))
+
 def riddle(arr):
-  for i in range(len(arr)):
-    size = i + 1
-    winMins = deque()
-    for j in range(len(arr) - i):
-      winMins.append(getMin(arr[j:j+size]))
-    yield getMax(list(winMins))
+  stack = deque()
+  max_window = {}
+  arr += [0,]
+  for i, n in enumerate(arr):
+    if not stack or n > stack[-1][1]:
+      stack.append((i, n))
+    else:
+      while stack and stack[-1][1] >= n:
+        if i-stack[-1][0] not in max_window or max_window[i-stack[-1][0]] < stack[-1][1]:
+          max_window[i-stack[-1][0]] = stack[-1][1]
+        p = stack.pop()
+      stack.append((p[0], n))
+  ans = [0]*(len(arr)-1)
+  v_ant = min(max_window.values())
+  for i in range(len(arr)-1, 0, -1):
+    if i in max_window and max_window[i] > v_ant:
+      ans[i-1] = max_window[i]
+      v_ant = max_window[i]
+    else:
+      ans[i-1] = v_ant
+  return ans
+
 
 if __name__ == '__main__':
   print(list(riddle([1, 2, 3, 5, 1, 13, 3])))

127d2df29faf82bef10d467cb3682bf09b734ada
Du Yuyang <du.r.yuyang@gmail.com>
2019-03-12 20:43:36 +1100

    use stack

--- data_structure/min_max_riddle.py
+++ data_structure/min_max_riddle.py
@@ -2,22 +2,21 @@ import heapq
 from collections import deque
 # Given an integer array of size , find the maximum of the minimum(s) of every window size in the array. The window size varies from 1 to n.
 
-def getMinWindow(arr):
+def getMin(arr):
   heapq.heapify(arr)
-  return heapq.heappop(arr)
+  return heapq.nsmallest(1, arr)[0]
 
+def getMax(arr):
+  heapq.heapify(arr)
+  return heapq.nlargest(1, arr)[0]
 
 def riddle(arr):
-  maxlist = []
-
   for i in range(len(arr)):
     size = i + 1
-    winMins = []
+    winMins = deque()
     for j in range(len(arr) - i):
-      winMins.append(getMinWindow(arr[j:j+size]))
-    maxlist.append(max(winMins))
-  return maxlist
-    
+      winMins.append(getMin(arr[j:j+size]))
+    yield getMax(list(winMins))
 
 if __name__ == '__main__':
-  print(riddle([1, 2, 3, 5, 1, 13, 3]))
+  print(list(riddle([1, 2, 3, 5, 1, 13, 3])))

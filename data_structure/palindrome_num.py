b050cb395dd1506673f1c6d9bb3238e93ce17988
Du Yuyang <du.r.yuyang@gmail.com>
2019-03-15 16:47:08 +1100

    my solution

---
+++ data_structure/palindrome_num.py
@@ -0,0 +1,39 @@
+# Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+# Example 1:
+
+# Input: 121
+# Output: true
+# Example 2:
+
+# Input: -121
+# Output: false
+# Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+# Example 3:
+
+# Input: 10
+# Output: false
+# Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+from collections import deque
+
+class Solution(object):
+    def isPalindrome(self, x):
+      stack = deque()
+      stack.extend(list(str(x)))
+      if x < 0:
+        return False
+      for i in range(len(stack)//2):
+        if stack.pop() != stack.popleft():
+          return False
+      return True
+
+
+
+
+if __name__ == '__main__':
+  s = Solution()
+  print(s.isPalindrome(1221))
+  print(s.isPalindrome(12321))
+  print(s.isPalindrome(-12321))
+  print(s.isPalindrome(123241))

db18ff4b142a7014c808222fbd564290fb301857
Du Yuyang <du.r.yuyang@gmail.com>
2019-03-23 14:17:55 +1100

    quick sort

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -1,7 +1,15 @@
 # QuickSort
 
-# Picks an element as pivot and partitions the given array around the picked pivot.
-# partition(), given an array and an element x of array as pivot, put x at its correct position in sorted array and put all smaller elements (smaller than x) before x, and put all greater elements (greater than x) after x
+# Stage 1
+# - Pick an element to be the pivot. In practice, this is usually the first or final element in the array.
+# 
+# Stage 2
+# - Go through the array, comparing every value to the pivot.
+# - Values less than the pivot should be moved before it, while values greater than the pivot should come after it. This is called the partition operation.
+# - You are left with two sub-arrays: one with numbers greater than the pivot value; the other with numbers less than it.
+# Stage 3
+# - Recursively apply steps 1 and 2 to each sub-array and repeat, until you can create no further sub-arrays.
+# - At that point, the sub-arrays are joined back into a single array.
 
 def partition(arr, low, high):
   # choose the last element as pivot
@@ -22,3 +30,23 @@ def partition(arr, low, high):
   arr[i+1], arr[high] = arr[high], arr[i+1]
   return i+1
   
+
+def quicksort(arr) {
+  # If a sub-array has only 1 value, the array is sorted and can be returned
+  if (len(arr) <= 1) {
+    return arr;
+  }
+  # Define pivot, and create empty "left" and "right" sub-arrays
+  pivot = arr[0]
+  left = []
+  right = []
+  # Go through the array, comparing values to the pivot, and sorting them into the "left" and "right" sub-arrays
+  for i in range(len(arr)):
+    if arr[i] < pivot:
+      left.append(arr[i])
+    else:
+      right.append(arr[i])
+    
+  # Repeat using recursion
+  return quicksort(left).extend(pivot, quicksort(right))
+}

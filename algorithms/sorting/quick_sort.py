b93f154d0fd543fb5697b1d4a616029f4bacdb45
Du Yuyang <du.r.yuyang@gmail.com>
2019-03-23 15:04:38 +1100

    fix the code to implement quick sort

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -7,46 +7,31 @@
 # - Go through the array, comparing every value to the pivot.
 # - Values less than the pivot should be moved before it, while values greater than the pivot should come after it. This is called the partition operation.
 # - You are left with two sub-arrays: one with numbers greater than the pivot value; the other with numbers less than it.
+#
 # Stage 3
 # - Recursively apply steps 1 and 2 to each sub-array and repeat, until you can create no further sub-arrays.
 # - At that point, the sub-arrays are joined back into a single array.
 
-def partition(arr, low, high):
-  # choose the last element as pivot
-  pivot = arr[high]
-
-  # initiate the index of smaller element
-  i = low - 1
-
-  # loop from from low to high - 1
-  for j in range(low, high):
-    # if current el is <= pivot
-    if arr[j] <= pivot:
-      i += 1
-      # swap arr[i] and arr[j]
-      arr[i], arr[j] = arr[j], arr[i]
-  
-  # swap arr[i+1] and arr[high]
-  arr[i+1], arr[high] = arr[high], arr[i+1]
-  return i+1
-  
-
-def quicksort(arr) {
+def quicksort(arr):
   # If a sub-array has only 1 value, the array is sorted and can be returned
-  if (len(arr) <= 1) {
-    return arr;
-  }
+  if len(arr) <= 1:
+    return arr
+
   # Define pivot, and create empty "left" and "right" sub-arrays
   pivot = arr[0]
   left = []
   right = []
   # Go through the array, comparing values to the pivot, and sorting them into the "left" and "right" sub-arrays
   for i in range(len(arr)):
+    # Ensure pivot itself is not in left or right
     if arr[i] < pivot:
       left.append(arr[i])
-    else:
+    elif arr[i] > pivot:
       right.append(arr[i])
     
   # Repeat using recursion
-  return quicksort(left).extend(pivot, quicksort(right))
-}
+  return quicksort(left) + [pivot] + quicksort(right)
+
+if __name__ == '__main__':
+  a = quicksort([6, 4, 8, 2, 10, 9])
+  print(a)

341b8816246b82a36aaa0ce10209dee7d800427f
Du Yuyang <du.r.yuyang@gmail.com>
2019-03-26 20:31:07 +1100

    get it to work

--- algorithms/sorting/merge_sort.py
+++ algorithms/sorting/merge_sort.py
@@ -9,16 +9,16 @@ def merge(arr, l, m, r):
     R = [0] * n2 
   
     # Copy data to temp arrays L[] and R[] 
-    for i in range(n1):
+    for i in range(n1): 
         L[i] = arr[l + i] 
   
-    for j in range(n2):
-        R[j] = arr[m + j]
+    for j in range(n2): 
+        R[j] = arr[m + 1 + j] 
   
     # Merge the temp arrays back into arr[l..r] 
     i = 0     # Initial index of left
-    j = 0     # Initial index of right 
-    k = l     # Initial index of merged arr 
+    j = 0     # Initial index of right
+    k = l     # Initial index of merged
   
     while i < n1 and j < n2 : 
         if L[i] <= R[j]: 
@@ -29,36 +29,37 @@ def merge(arr, l, m, r):
             j += 1
         k += 1
   
-    # # Copy the remaining elements of L[], if there 
-    # # are any 
-    # while i < n1: 
-    #     arr[k] = L[i] 
-    #     i += 1
-    #     k += 1
+    # Copy the remaining elements of L[], if there 
+    # are any 
+    while i < n1: 
+        arr[k] = L[i] 
+        i += 1
+        k += 1
   
-    # # Copy the remaining elements of R[], if there 
-    # # are any 
-    # while j < n2: 
-    #     arr[k] = R[j] 
-    #     j += 1
-    #     k += 1
+    # Copy the remaining elements of R[], if there 
+    # are any 
+    while j < n2: 
+        arr[k] = R[j] 
+        j += 1
+        k += 1
 
 def merge_sort(arr, l, r):
     if l < r:
         # if left < right index
         # devide list into half
         m = (l + r) // 2
-        larr = arr[l:m]
-        rarr = arr[m+1:r]
+        # larr = arr[l:m]
+        # rarr = arr[m+1:r]
         # merge_sort the left half
-        merge_sort(larr, l, m)
+        merge_sort(arr, l, m)
         # merge_sort the right half
-        merge_sort(rarr, m+1, r)
+        merge_sort(arr, m+1, r)
         # merge the left and right
         merge(arr, l, m, r)
 
 a = [12, 46, 2, 8, 11, 6, 7, 9, 17]
 
-merge_sort(a, 0, len(a))
+n = len(a)
+merge_sort(a, 0, n-1)
 
 print(a)

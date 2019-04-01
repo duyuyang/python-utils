21ad24d371f6c8b0f92248479eee365984d10639
Du Yuyang <du.yuyang@rea-group.com>
2019-04-01 21:47:07 +1100

    add insert sort to actually sort unsorted

--- algorithms/sorting/insertion_sort.py
+++ algorithms/sorting/insertion_sort.py
@@ -4,13 +4,14 @@ def insertionSort1(n, arr):
   e = arr[n-1]
   while arr[n-2] > e and n > 1:
     arr[n-1] = arr[n-2]
-    print(" ".join(map(str, arr)))
+    # print(" ".join(map(str, arr)))
     n -= 1
   arr[n-1] = e
-  print(" ".join(map(str, arr)))
+  return arr
+  # print(" ".join(map(str, arr)))
 
-arr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
-insertionSort1(10, arr)
+# arr = [2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
+# insertionSort1(10, arr)
 
 # expect output
 # 2 3 4 5 6 7 8 9 10 10
@@ -23,3 +24,14 @@ insertionSort1(10, arr)
 # 2 3 3 4 5 6 7 8 9 10
 # 2 2 3 4 5 6 7 8 9 10
 # 1 2 3 4 5 6 7 8 9 10
+
+
+def insertionSort2(n, arr):
+
+  for i in range(1, len(arr)):
+    arr[0:i+1] = insertionSort1(i+1, arr[0:i+1])
+    print(" ".join(map(str, arr)))
+
+
+arr = [1, 4, 3, 5, 6, 2]
+insertionSort2(6, arr)
